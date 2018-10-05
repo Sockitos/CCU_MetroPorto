272c8a6e9d5feb5b4a48709d3bc9224a268ff6d0
Rodrigo Oliveira <rodrigdoliveira@hotmail.com>
2018-10-05 23:27:58 +0100

    box moving yey

--- website/javascript.js
+++ website/javascript.js
@@ -1,9 +1,27 @@
 $( document ).ready(function() {
+
   $(".profile-image").mouseenter(function(){
     $("#lateral-bar").css("width", "11vw");
+    $(this).css("filter", "grayscale(0%)");
+
+    current_profile = this.id.split('-')[1];
+    student_box_pos = 1 + parseInt(this.id.split('-')[2])*(7);
+
+    image_url = "img/"+current_profile+".jpg"
+
+    $(this).css("width", "8vw");
+    $(this).css("height", "8vw");
+    $("#student-box").css({top: student_box_pos.toString()+"vw"});
+    $("#student-box").show();
+
+    $("#student-box-picture").css("background-image", "url("+image_url+")");
   });
 
   $(".profile-image").mouseleave(function(){
+    $(this).css("width", "6vw");
+    $(this).css("height", "6vw");
     $("#lateral-bar").css("width", "9vw");
+    $(this).css("filter", "grayscale(100%)");
+    $("#student-box").hide();
   });
 });
